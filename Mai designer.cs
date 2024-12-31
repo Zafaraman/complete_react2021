6cc20720b97394e3a6d7a2a23da75f02b37bd9be
Zafar Aman <87444423+Zafaraman@users.noreply.github.com>
2024-12-31 17:09:34 +0530

    Update Mai designer.cs

--- Mai designer.cs
+++ Mai designer.cs
@@ -1,8 +1,39 @@
-document.querySelectorAll('.hf-error').forEach(function (element) {
-    const value = parseFloat(element.getAttribute('data-value')); // Get the value
-    if (value === 0) {
-        element.classList.add('red'); // Apply red if 0
-    } else {
-        element.classList.add('green'); // Apply green for other values
-    }
-});
+    <div class="parameter-row">
+      <div class="parameter-label">
+          <span>Tube Mill </span>
+          <span class="unit-label"></span>
+          @* <span>MILL CONDITION</span>
+          <span class="unit-label">START/STOP</span> *@
+      </div>
+       
+      <div class="readings-heading">
+          <div class="TM1reading-box">
+              TM1 
+              
+          </div>
+          <div class="TM2reading-box">TM2</div>
+          <div class="TM3reading-box">TM3</div>
+          <div class="TM4reading-box">TM4</div>
+          <div class="TM7reading-box">TM7</div>
+      </div>
+      
+  </div>
+  <div class="readings-heading">
+      <div class="TM1reading-box hf-error" data-value="@Model.HFError.TM1Value.ToString()">
+          HF
+        
+      </div> 
+     
+      <div class="TM2reading-box hf-error" data-value="@Model.HFError.TM2Value.ToString()">
+          HF
+      </div>
+      <div class="TM3reading-box hf-error" data-value="@Model.HFError.TM3Value.ToString()">
+          HF
+      </div>
+      <div class="TM4reading-box hf-error" data-value="@Model.HFError.TM4Value.ToString()">
+          HF
+      </div>
+      <div class="TM7reading-box hf-error" data-value="@Model.HFError.TM7Value.ToString()">
+          HF
+      </div>
+  </div>
